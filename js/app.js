d298f9e1facb24d2ec442ee2bd7a9604ea441dcf
Jubayer <jubayer98@gmail.com>
2024-09-28 11:05:16 +0200

    seventh commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-function processDonation(donationInputId, cardAmountId) 
+function processDonation(donationInputId, cardAmountId, cardHeaderId) 
 {
     const donationAmount = document.getElementById(donationInputId);
     const accountBalance = document.getElementById('accountBalance');
     const cardAmount = document.getElementById(cardAmountId);
 
     let donationInputValue = parseFloat(donationAmount.value);
+    let cardHeader = document.getElementById(cardHeaderId).innerHTML;
 
     // validate the input to ensure it is a positive number
     if (isNaN(donationInputValue) || donationInputValue <= 0) 
@@ -27,23 +28,36 @@ function processDonation(donationInputId, cardAmountId)
     accountBalance.textContent = currentAccountBalance - donationInputValue;
     cardAmount.textContent = parseFloat(cardAmount.textContent) + donationInputValue;
 
+    appendDonationHistory(donationInputValue, cardHeader);
+
     donationAmount.value = "";
+}
+
+
+function appendDonationHistory(amount, cardHeader) {
+    const historySection = document.getElementById('historySection');
+    const newEntry = document.createElement('div');
+    newEntry.className = "space-y-3 mt-3 rounded-lg border border-gray-300 px-5 py-5";
+    newEntry.innerHTML = `<h2 class="font-bold text-xl">${amount} Taka is ${cardHeader}</h2>
+                          <p>Date : Tue Sep 17 2024</p>`;
+    historySection.appendChild(newEntry);
+    historySection.classList.add('hidden'); // Show the history section if hidden
 }
 
 // attach event listeners to buttons
 document.getElementById('donationButton1').addEventListener('click', function() 
 {
-    processDonation('donationAmount1', 'card1Amount');
+    processDonation('donationAmount1', 'card1Amount', 'card1Header');
 });
 
 document.getElementById('donationButton2').addEventListener('click', function() 
 {
-    processDonation('donationAmount2', 'card2Amount');
+    processDonation('donationAmount2', 'card2Amount', 'card2Header');
 });
 
 document.getElementById('donationButton3').addEventListener('click', function() 
 {
-    processDonation('donationAmount3', 'card3Amount');
+    processDonation('donationAmount3', 'card3Amount', 'card3Header');
 });
 
 
@@ -75,4 +89,3 @@ function toggleSection(activeSection)
         historyBtn.classList.add('bg-lime-300');
     }
 }
-  
